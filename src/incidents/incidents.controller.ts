489402f88698e9a5211cbc30edc65a3fb7a9b6cf
Juan Frausto <juanfr97@hotmail.com>
2026-03-20 17:16:37 -0600

    Cache implemented

--- src/incidents/incidents.controller.ts
+++ src/incidents/incidents.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, ParseFloatPipe, Post, Query } from '@nestjs/common';
 import { IncidentsService } from './incidents.service';
 import type { IncidentCDto } from 'src/core/interfaces/incident.interface';
 
@@ -7,6 +7,22 @@ export class IncidentsController {
 
     constructor(private readonly incidentService:IncidentsService){}
 
+    @Get()
+    async getIncidents(){
+        const result = await this.incidentService.getIncidents();
+        return result;
+    }
+
+    //Cuando quieres filtrar ---> Query String ?radius=500&title=hola
+    @Get('radius')
+    async getIncidentByRadius(
+        @Query('lat',ParseFloatPipe) lat : number,
+        @Query('lon',ParseFloatPipe) lon : number,
+        @Query('radiusInMeters',ParseFloatPipe) radiusInMeters : number
+    ){
+        const result = await this.incidentService.getIncidentByRadius(lat,lon,radiusInMeters);
+        return result;
+    }
     @Post()
     async createIncident(@Body() incident: IncidentCDto){
         const result = await this.incidentService.createIncident(incident);
--- src/incidents/incidents.service.ts
+++ src/incidents/incidents.service.ts
@@ -6,8 +6,14 @@ import { generateIncidentEmailTemplate } from './templates/incident-email.templa
 import { Repository } from 'typeorm';
 import { Incident } from 'src/core/db/entities/incident.entity';
 import { InjectRepository } from '@nestjs/typeorm';
+import { logger } from 'src/config/logger';
+import Redis from 'ioredis';
+import { envs } from 'src/config/envs';
 // npm run start:dev
-//Repositorio --> Patron de Diseño Repository
+//Repositorio --> Patron de Diseño Repository JWT - Redis - PDF - AppInsights
+
+const CACHE_KEY_ALL_INCIDENTS = 'incidents:all';
+
 @Injectable()
 export class IncidentsService {
     constructor(
@@ -15,6 +21,63 @@ export class IncidentsService {
         private readonly incidentRepository : Repository<Incident>,
         private readonly emailService : EmailService
     ){}
+// Cloud Watch / App Insights txt / Keyvault
+// logger.info
+    private readonly redis = new Redis({
+        host:envs.REDIS_HOST,
+        port:envs.REDIS_PORT
+    });
+
+    async getIncidentByRadius(lat:number,lon:number,radius:number) : Promise<Incident[]>{
+        //SP
+        try{
+            logger.info(`Buscando incidentes en ${lat}, ${lon} en un radio de ${radius} m`)
+            const incidents = await this.incidentRepository
+                    .createQueryBuilder('incident')
+                    .where(`
+                       ST_DWithin(
+                        incident.location::geography,
+                        ST_SetSRID(ST_MakePoint(:lon, :lat), 4326)::geography,
+                        :radius
+                    )    
+                    `,{ lon,lat,radius })
+                    .getMany();
+            logger.info(`${incidents} incidentes encontrados en un radio de ${radius} m`)
+            return incidents;
+        }
+        catch(error){
+            console.error(error);
+            return [];
+        }
+    }
+
+    async getIncidents() : Promise<Incident[]>{
+        try{
+            logger.info("[IncidentService] Consultando incidentes en cache...");
+            const data = await this.redis.get(CACHE_KEY_ALL_INCIDENTS) ?? "[]";
+            const cacheIncidents = JSON.parse(data) as Incident[];
+            
+            if(cacheIncidents.length > 0){
+                logger.info("[IncidentService] Incidentes en cache")
+                return cacheIncidents;
+            }
+            //JSON.PARSE
+            //JSON.stringify
+            logger.info("[IncidentService] Trayendo todos los incidentes...");
+            const incidents = await this.incidentRepository.find();
+            logger.info("[IncidentService] Guardando incidentes en cache");
+            const incidentsString = JSON.stringify(incidents);
+            this.redis.set(CACHE_KEY_ALL_INCIDENTS,incidentsString)
+            logger.info(`[IncidentService] Se obtuvieron ${incidents.length} incidentes`)
+            return incidents;
+        }
+        catch(error){
+            console.error("[IncidentService] Error al traer los incidentes");
+            console.error(error);
+            return [];
+        }
+        
+    }
 
     async createIncident(incident:IncidentCDto) : Promise<Boolean>{
         // UNA ENTIDAD DE INICIDENTE
@@ -28,7 +91,10 @@ export class IncidentsService {
                 coordinates:[incident.lon, incident.lat]
             }
         });
+        logger.info("Creando Incidente");
         await this.incidentRepository.save(newIncident);
+        await this.redis.del(CACHE_KEY_ALL_INCIDENTS);
+        logger.info("Mandando correo");
         const template = generateIncidentEmailTemplate(incident);
         const options : EmailOptions = {
             to: "devjdfr@gmail.com",
